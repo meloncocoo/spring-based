2a592235c33aa93003f3f881b8f4d698009f3fc2
Melon <meloncocoo@gmail.com>
2016-01-29 17:58:39 +0800

    -add grunt for less compile

--- web-ui/grunt/clean.js
+++ web-ui/grunt/clean.js
@@ -1,9 +1,10 @@
-module.exports = function(grunt) {
-	"use strict";
-	
-	return {
-		css:		'<%= config.destination.css %>',
-		js:			'<%= config.destination.js %>',
-		skins:		'<%= config.destination.skins %>'
-	}
-}
+module.exports = function () {
+  "use strict";
+
+  return {
+    css: '<%= config.destination.css %>',
+    js: '<%= config.destination.js %>',
+    fonts: '<%= config.destination.fonts %>/*/*.css',
+    vendor: '<%= config.destination.vendor %>/*/*.css'
+  };
+};

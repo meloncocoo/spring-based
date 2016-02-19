5edf301ee6aa014b383081b6240e15f48d29268a
Melon <meloncocoo@gmail.com>
2016-02-19 12:08:09 +0800

    +updated

---
+++ web-boot/src/main/java/org/melon/spring/domain/util/FixedH2Dialect.java
@@ -0,0 +1,14 @@
+package org.melon.spring.domain.util;
+
+import java.sql.Types;
+
+import org.hibernate.dialect.H2Dialect;
+
+public class FixedH2Dialect extends H2Dialect {
+
+	public FixedH2Dialect() {
+		super();
+		registerColumnType(Types.FLOAT, "real");
+	}
+	
+}

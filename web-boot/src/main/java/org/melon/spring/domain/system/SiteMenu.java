ad8af47ca547c6bc8094f1ca08b32357141729ce
meloncocoo <nimda_wen@hotmail.com>
2016-05-14 21:54:37 +0800

    add mkc-ember-ui

--- web-boot/src/main/java/org/melon/spring/domain/system/SiteMenu.java
+++ web-boot/src/main/java/org/melon/spring/domain/system/SiteMenu.java
@@ -3,7 +3,13 @@ package org.melon.spring.domain.system;
 public class SiteMenu extends Menu {
 
 	private String icon;
-	
-	
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
 	
 }
